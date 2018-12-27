092612fde1173d1d81d6b292bb06b7c7a3df89c8
Jonas0801 <46156216+Jonas0801@users.noreply.github.com>
2018-12-27 14:57:19 +0800

    Create EnumTest.cs

---
+++ EnumTest.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumTest : MonoBehaviour {
+
+    public string[] ansValue;
+    int count;
+    public enum ans
+    {
+        A, B, C, D
+    };
+
+    public ans myAns;
+    private void isAns()
+    {
+        if (count == ansValue.Length)
+        {
+            count = 0;
+            print("過關");
+        }
+
+        if (ansValue[count] == myAns.ToString())
+        {
+            print("正確答案是" + myAns);
+            count++;            
+        }
+        else
+            print("錯誤");       
+    }
+
+    public void A()
+    {
+        myAns = ans.A;
+        isAns();
+    }
+    public void B()
+    {
+        myAns = ans.B;
+        isAns();
+    }
+    public void C()
+    {
+        myAns = ans.C;
+        isAns();
+    }
+    public void D()
+    {
+        myAns = ans.D;
+        isAns();
+    }
+}
+
+
